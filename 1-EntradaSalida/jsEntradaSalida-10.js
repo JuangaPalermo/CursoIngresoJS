ec7b00ea448b7aad39b13c0563a18372ee07ffc6
JuangaPalermo <juangabrielpalermo@gmail.com>
2020-02-05 20:45:28 -0300

    Fin de ejercicios modulo 1

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,12 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    var importe;
+    var descuento;
+    
+    importe = parseInt(document.getElementById("importe").value);
+
+    descuento = (importe*75/100);
+
+    document.getElementById("resultado").value = descuento;	
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,17 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var sueldo;
+    var aumento;
+    
+    sueldo = parseInt(document.getElementById("sueldo").value);
 
-	
+    aumento = (sueldo*110/100);
+
+    document.getElementById("resultado").value = aumento;
+
+    // tambien puedo limitar la cantidad de decimales agregandole a la variable .toFixed(CantidadDeDecimalesQueQuiero)
+    // por ejemplo podria hacer:
+    // aumento = sueldo * 1.1
+    // document.getElementById("resultado").value = aumento.toFixed(2)
 }
