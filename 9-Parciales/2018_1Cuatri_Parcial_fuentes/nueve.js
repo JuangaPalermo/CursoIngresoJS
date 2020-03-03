c5dc991430c49830e400581d91c9144c455f30aa
JuangaPalermo <juangabrielpalermo@gmail.com>
2020-03-03 17:55:01 -0300

    Simulacros parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,99 @@
+/*Bienvenidos.
+Realizar el algoritmo que permita ingresar la marca del producto, el peso el cual debe ser entre 1 y 100 
+(validar), la temperatura de almacenamiento(entre -30 y 30) hasta que el usuario quiera e informar al terminar
+el ingreso por document.write:
+
+- a) La cantidad de temperaturas pares.
+- b) La marca del producto más pesado
+- c) La cantidad de productos que se conservan a menos de 0 grados.
+- d) El promedio del peso de todos los productos.
+- f) El peso máximo y el mínimo.
+
+Testeo con los siguientes valores
+(m=pepito;p = 50; t =-300(mal) ,-30(bien) )
+(m=teem;p = 10; t =0 )
+(m=llut;p = 150(mal), 15(bien); t =-13 )
+(m=fpy;p = 45; t =-12 )*/
+
 function mostrar()
-{
+{   
+    var marca;
+    var peso;
+    var temperatura;
+    var cantTemperaturasPares = 0;
+    var marcaProductoMasPesado;
+    var cantAlmacenamientoBajoCero = 0;
+    var promedioPesos;
+    var sumaPesos = 0;
+    var cantProductos = 0;
+    var pesoMaximo;
+    var pesoMinimo;
+    var flag = 0;
+
+    do{
+        //INGRESO DE DATOS
+        marca = prompt("Ingrese la marca del producto: ");
+
+        peso = parseInt(prompt("Ingrese el peso del producto: "));
+
+        while (peso < 1 || peso > 100){
+
+            peso = parseInt(prompt("Ingresó un peso inválido, inténtelo nuevamente: "));
+        } 
+
+        temperatura = parseInt(prompt("Ingrese la temperatura de almacenamiento del producto: "));
+
+        while (temperatura < -30 || temperatura > 30){
+
+            temperatura = parseInt(prompt("Ingresó una temperatura inválida, inténtelo nuevamente: "));
+        }
+
+        // PUNTO A:
+        if (temperatura % 2 == 0){
+
+            cantTemperaturasPares++;
+        }
+
+        //PUNTO B y F:
+
+        if (flag == 0 || peso > pesoMaximo){
+
+            pesoMaximo = peso;
+            marcaProductoMasPesado = marca;
+        }
+
+        if (flag == 0 || peso < pesoMinimo){
+
+            pesoMinimo = peso;
+            flag = 1
+        }
+
+        //PUNTO C: 
+
+        if (temperatura < 0){
+
+            cantAlmacenamientoBajoCero++;
+        }
+
+        //PUNTO D (1):
+
+        sumaPesos = sumaPesos + peso;
+        cantProductos++;
+        
+        respuesta = prompt("Desea seguir ingresando articulos? si/no: ")
+
+       
+    } while (respuesta == "si");
+
+        //PUNTO D (2):
+
+        promedioPesos = sumaPesos / cantProductos;
+
+    document.write("Cantidad de temperaturas pares: " + cantTemperaturasPares + "</br>");
+    document.write("Marca del producto mas pesado: " + marcaProductoMasPesado + "</br>");
+    document.write("Cantidad de productos que se almacenan a menos de 0 grados: " + cantAlmacenamientoBajoCero + "</br>");
+    document.write("Promedio del peso de todos los productos: " + promedioPesos + "</br>");
+    document.write("Peso maximo: " + pesoMaximo + "</br>");
+    document.write("Peso minimo: " + pesoMinimo + "</br>");
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,104 @@
+/* Realizar el algoritmo que permita iterar el ingreso de dos datos, una letra y un número entre -100 y 100 
+(validar) hasta que el usuario quiera e informar al terminar el ingreso por document.write:
+
+- a) La cantidad de números pares. 
+- b) La cantidad de números impares.
+- c) La cantidad de ceros.
+- d) El promedio de todos los números positivos ingresados.
+- e) La suma de todos los números negativos.
+- f) El número y la letra del máximo y el mínimo.
+
+Testeo con los siguientes datos
+( d , -150(mal) , -50(bien))
+( z , 0)
+( g , 20)
+( a , 150(mal) , 0(bien))
+( b , 3)
+( k , 7)*/
+
 function mostrar()
 {
+    var numero;
+    var letra;
+    var respuesta;
+    var cantidadPares = 0;
+    var cantidadImpares = 0;
+    var cantidadCeros = 0;
+    var sumaPositivos = 0;
+    var cantidadPositivos = 0;
+    var promedioPositivos;
+    var sumaNegativos = 0;
+    var letraMaxima;
+    var numeroMaximo;
+    var letraMinima;
+    var numeroMinimo;
+    var flag = 0;
+
+    do {
+
+        //INGRESO DE DATOS
+        letra = prompt("Ingrese la letra que desee, de la A a la Z: ");   
+        
+        numero = parseInt(prompt("Ingrese el numero que desee, entre -100 y 100: "));
+
+        while (numero < -100 || numero > 100 || isNaN(numero)){
+            numero = parseInt(prompt("El numero ingresado es invalido, intente nuevamente (entre -100 y 100): "));
+        }
+
+        //VERIFICACION PAR-IMPAR-CERO
+        if (numero % 2 == 0 && numero != 0){
+
+            cantidadPares++;
+
+        } else if (numero % 2 != 0){
+
+            cantidadImpares++;
+
+        } else if (numero == 0){
+
+            cantidadCeros++;
+
+        }
+
+        //VERIFICACION POSITIVO/NEGATIVO
+        if (numero >= 0){
+
+            sumaPositivos = sumaPositivos + numero;
+            cantidadPositivos++;
+
+        } else {
+
+            sumaNegativos = sumaNegativos + numero;
+
+        }
+
+        //ALMACENAMIENTO MAXIMO/MINIMO
+        if (flag == 0 || numeroMaximo < numero){
+
+            numeroMaximo = numero;
+            letraMaxima = letra;
+        }
+
+        if (flag == 0 || numeroMinimo > numero){
+
+            numeroMinimo = numero;
+            letraMinima = letra;
+            flag = 1;
+        }
+
+        respuesta = prompt("¿Desea seguir añadiendo numeros y letras? si/no: ");
+    } while (respuesta == "si")
+
+    promedioPositivos = sumaPositivos / cantidadPositivos;
+
+    //MUESTRA DE RESULTADOS
+    document.write("La cantidad de números pares (sin contar los ceros) es: " + cantidadPares + "</br>");
+    document.write("La cantidad de números impares es: " + cantidadImpares + "</br>");
+    document.write("La cantidad de ceros es: " + cantidadCeros + "</br>");
+    document.write("El promedio de todos los numeros positivos ingresados es: " + promedioPositivos + "</br>");
+    document.write("La suma de todos los numeros negativos ingresados es: " + sumaNegativos + "</br>");
+    document.write("El numero maximo es: " + numeroMaximo + " y su letra es: " + letraMaxima + "</br>");
+    document.write("El numero minimo es: " + numeroMinimo + " y su letra es: " + letraMinima + "</br>");
+
 
 }
